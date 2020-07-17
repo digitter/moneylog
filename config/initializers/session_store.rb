bbd997c42ded26afd3aa4c7df9f092a27b07af39
kazuki <matchingserver@gmail.com>
2020-07-17 20:59:29 +0900

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,7 @@
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: '_app_session', domain: "production-domain"
+else
+  Rails.application.config.session_store :cookie_store, key: '_app_session'
+end
+
+Rails.application.config.api_only = false
