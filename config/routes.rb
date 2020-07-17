ce02a7cceee922a5de32db3f783cf90644dd5fc6
kazuki <matchingserver@gmail.com>
2020-07-17 22:14:59 +0900

    modify routes for registration and session

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      resources :registartions, only: %i[create]
       resources :sessions, only: %i[create destroy]
+      delete :logout, to: "sessions#logout"
+      get :logged_in, to: "sessions#logged_in"
     end
   end
 
