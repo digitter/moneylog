374a31a033cabcb49db1a4fe041e0d4ec06648fe
kazuki <matchingserver@gmail.com>
2020-07-17 21:57:17 +0900

    fix typo

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     user = User
-            .find_by(email: parasm[:user][:email])
-            .try(:authenticate, param[:user][:password])
+            .find_by(email: params[:user][:email])
+            .try(:authenticate, params[:user][:password])
 
     if user
       session[:user_id] = user.id
