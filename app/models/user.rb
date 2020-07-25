1b0e4104c4b07cb8badc3edf7fda475486751f11
kazuki <matchingserver@gmail.com>
2020-07-25 11:17:51 +0900

    Add validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  has_secure_password
+  before_save { self.email = email.downcase }
 
-  validates :email, presence: true
-  validates :email, uniqueness: true
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
 end
