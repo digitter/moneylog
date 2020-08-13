9b27cea0b5c084f9b4cb19d19fb6e3616599729c
kazuki <matchingserver@gmail.com>
2020-08-14 08:10:33 +0900

    improve method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,11 @@ class ApplicationController < ActionController::API
   after_action :set_csrf_token
 
   def verify_csrf_token
-    # 認証トークンが不一致ならばセッションをリセット
-    # 認可が必要な場面ではcurrent user concernでユーザーが取得できるかを確認すれば良い
-    reset_session unless session[:auth_token] == request.headers['X-CSRF-Token']
+    # 認証トークンが不一致ならばセッションをリセット & リダイレクト
+    unless session[:auth_token] == request.headers['X-CSRF-Token']
+      reset_session
+      redirect_to api_v1_logged_in_path
+    end
   end
 
   def set_csrf_token
