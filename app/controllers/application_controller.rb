52f4046998a7f46e8673d0e355226d557129bbb1
kazuki <matchingserver@gmail.com>
2020-09-01 19:14:59 +0900

    set rfresh token if response success

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,21 @@ class ApplicationController < ActionController::API
 
   def verify_csrf_token
     # 認証トークンが不一致ならばセッションをリセット
+    # セッションがリセットされた後
+    # 認可チェックが必要な場合は authenticate_user! で制御する。
     reset_session unless session[:auth_token] == request.headers['X-CSRF-Token']
-  end
-
-  def set_csrf_token
-    session[:auth_token] = SecureRandom.urlsafe_base64
-    # FIXME: レスポンスヘルパーでエラーを起こした時にトークンが送信されない
-    response.set_header('X-CSRF-Token', session[:auth_token])
   end
 
   def authenticate_user!
     # ユーザーがログインしていないならばリダイレクト
     redirect_to api_v1_logged_in_path unless @current_user
+  end
+
+  def set_csrf_token
+    # カスタムヘッダが送信されるレスポンス成功時 (200~300) のみに新しいトークンの付与をする
+    if response.successful?
+      session[:auth_token] = SecureRandom.urlsafe_base64
+      response.set_header('X-CSRF-Token', session[:auth_token])
+    end
   end
 end
