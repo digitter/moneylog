e72c899da6766067da734a9354692b6528826067
kazuki <matchingserver@gmail.com>
2020-09-02 01:08:28 +0900

    improve user model

--- front_end/models/User.ts
+++ front_end/models/User.ts
@@ -1,31 +1,26 @@
-import JSONAPISerializer from "json-api-serializer"
-const Serializer = new JSONAPISerializer()
-
 export default class User {
   constructor(
     public name: string,
     public email: string,
-    public created_at: Date,
-    public updated_at: Date
+    public created_at?: Date,
+    public updated_at?: Date
   ) {}
 
-  static fromJson(jsonApiFormat: any): User {
-    if (jsonApiFormat.data.data.type !== 'user') { return null }
-
-    Serializer.register('user', jsonApiFormat)
-
+  static serialized(params: User): User {
     const {
       name,
       email,
-      created_at,
-      updated_at
-    } = Serializer.deserialize('user', jsonApiFormat)
+    } = params
 
     return new User(
       name,
       email,
-      created_at,
-      updated_at
     )
+  }
+
+  static fromJsonApi(jsonApiFormat: any): User {
+    if (jsonApiFormat.data.type !== 'user') { return null }
+
+    return jsonApiFormat.data.attributes
   }
 }
