d1d300495ac24e50ab2cbfcfd0589d59995ebc1f
kazuki <matchingserver@gmail.com>
2020-09-26 01:54:19 +0900

    improve model validation

--- app/models/expenditure_log.rb
+++ app/models/expenditure_log.rb
@@ -2,7 +2,7 @@ class ExpenditureLog < ApplicationRecord
   # validations
   validates :amount, presence: true
   validates :title, presence: true, length: { minimum: 1, maximum: 100 }
-  validates :content, length: { minimum: 1, maximum: 100 }
+  validates :content, length: { maximum: 100 }
 
   # association
   belongs_to :user
--- app/models/monthly_expenditure.rb
+++ app/models/monthly_expenditure.rb
@@ -2,8 +2,8 @@ class MonthlyExpenditure < ApplicationRecord
   # validations
   validates :amount, presence: true
   validates :title, presence: true, length: { minimum: 1, maximum: 100 }
-  validates :content, length: { minimum: 1, maximum: 100 }
-  validates :is_active, presence: true, inclusion: {in: [true, false]}
+  validates :content, length: { maximum: 100 }
+  validates :is_active, inclusion: {in: [true, false]}
 
   # association
   belongs_to :user
