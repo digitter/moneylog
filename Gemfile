a15a300c0321a440370dd78ae2a0655b6977ff4c
kazuki <matchingserver@gmail.com>
2020-09-29 00:17:38 +0900

    setup cron job

--- Gemfile
+++ Gemfile
@@ -50,3 +50,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'fast_jsonapi'
 
+gem 'whenever', require: false
--- app/models/monthly_expenditure.rb
+++ app/models/monthly_expenditure.rb
@@ -10,4 +10,33 @@ class MonthlyExpenditure < ApplicationRecord
   has_many :expenditure_logs
 
   # method
+  def self.create_expenditure_logs
+    User.includes(:monthly_expenditures).find_each do |user|
+      ActiveRecord::Base.transaction do
+        user.monthly_expenditures.each do |monthly_expenditure|
+          now = Time.zone.now
+          scheduled_date = monthly_expenditure.will_create_at
+
+          if (monthly_expenditure.is_active &&
+            scheduled_date.year == now.year &&
+            scheduled_date.month == now.month &&
+            scheduled_date.day == now.day)
+
+            log = monthly_expenditure.expenditure_logs.new(
+              user_id: monthly_expenditure.user_id,
+              title: monthly_expenditure.title,
+              amount: monthly_expenditure.amount,
+              content: monthly_expenditure.content)
+
+            log.save!
+          end
+        end
+      end
+
+      # rescue => exception
+        # puts "error"
+        # TODO: エラー後の処理
+    end
+  end
+
 end
