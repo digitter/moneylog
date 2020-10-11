30f8962f536f82d03ee2a588381fa7e258fd1c98
kazuki <matchingserver@gmail.com>
2020-10-12 01:57:02 +0900

    add spec

---
+++ spec/factories/tag_relations.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :tag_relation do
+    user { nil }
+    expenditure_log_id { "MyString" }
+    integer { "MyString" }
+    income_log { "MyString" }
+    integer { "MyString" }
+  end
+end
