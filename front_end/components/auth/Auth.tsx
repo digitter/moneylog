59bc91e1d78af7bde775e6b50f53e22fb7bddec4
kazuki <matchingserver@gmail.com>
2020-10-14 21:52:27 +0900

    set tags to store after user is authenticated

--- front_end/components/auth/Auth.tsx
+++ front_end/components/auth/Auth.tsx
@@ -11,15 +11,17 @@ import { editAssets } from '../../modules/AssetModule'
 import { editExpenditureLogs, actionTypes as expenditureActionTypes } from '../../modules/ExpenditureLogModule'
 import { editIncomeLogs, actionTypes as incomeActionTypes } from '../../modules/IncomeLogModule'
 import { editMonthlyExpenditures, actionTypes as monthlyExpenditureTypes } from '../../modules/MonthlyExpenditureModule'
+import { editTags, actionTypes as tagTypes } from '../../modules/TagModule'
 
 import User from '../../models/User'
 import Asset from '../../models/Asset'
 import ExpenditureLog from '../../models/ExpenditureLog'
 import IncomeLog from '../../models/IncomeLog'
+import MonthlyExpenditure from '../../models/MonthlyExpenditure'
+import Tag from '../../models/Tag'
 
 import LoadingIcon from '../LoadingIcon'
 import Top from '../Top'
-import MonthlyExpenditure from '../../models/MonthlyExpenditure'
 
 interface Props {
   history: any
@@ -32,6 +34,7 @@ interface Props {
   editExpenditureLogs: typeof editExpenditureLogs
   editIncomeLogs: typeof editIncomeLogs
   editMonthlyExpenditures: typeof editMonthlyExpenditures
+  editTags: typeof editTags
 }
 interface State {
   loggedInStatus: string
@@ -52,6 +55,7 @@ class Auth extends React.Component<Props, State> {
             this.props.editExpenditureLogs(expenditureActionTypes.initialize, ExpenditureLog.fromIncluded(jsonApiFormat))
             this.props.editIncomeLogs(incomeActionTypes.initialize, IncomeLog.fromIncluded(jsonApiFormat))
             this.props.editMonthlyExpenditures(monthlyExpenditureTypes.initialize, MonthlyExpenditure.fromIncluded(jsonApiFormat))
+            this.props.editTags(tagTypes.initialize, Tag.fromIncluded(jsonApiFormat))
 
             this.setState({ loggedInStatus: 'LOGGED_IN' })
           }
@@ -61,6 +65,8 @@ class Auth extends React.Component<Props, State> {
             this.props.editExpenditureLogs(expenditureActionTypes.reset, [])
             this.props.editIncomeLogs(incomeActionTypes.reset, [])
             this.props.editMonthlyExpenditures(monthlyExpenditureTypes.reset, [])
+            this.props.editTags(tagTypes.reset, [])
+
             this.setState({ loggedInStatus: 'NOT_LOGGED_IN' })
           }
         })
@@ -70,8 +76,9 @@ class Auth extends React.Component<Props, State> {
           this.props.editExpenditureLogs(expenditureActionTypes.reset, [])
           this.props.editIncomeLogs(incomeActionTypes.reset, [])
           this.props.editMonthlyExpenditures(monthlyExpenditureTypes.reset, [])
-          this.setState({ loggedInStatus: 'NOT_LOGGED_IN' })
+          this.props.editTags(tagTypes.reset, [])
 
+          this.setState({ loggedInStatus: 'NOT_LOGGED_IN' })
           console.error(error)
         })
     }
@@ -108,7 +115,8 @@ const mapDispatchToProps = dispatch => {
       editAssets,
       editExpenditureLogs,
       editIncomeLogs,
-      editMonthlyExpenditures
+      editMonthlyExpenditures,
+      editTags
     },
     dispatch
   )
