0ed2afe1cdff3e8cdc9daa34e453345e10fdf407
kazuki <matchingserver@gmail.com>
2020-10-18 21:13:11 +0900

    reflect tag to store after updating it

--- front_end/components/tags/EditingTagModal.tsx
+++ front_end/components/tags/EditingTagModal.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
+import { useDispatch } from 'react-redux'
 import Tag from "../../models/Tag";
 import TextField from '@material-ui/core/TextField';
-import LocalOfferIcon from '@material-ui/icons/LocalOffer';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper';
 import SketchExample from "./ScketchExample";
+import { updateTag } from "../../services/TagService";
+import { successMessage, succesmMessages, errorMessage, errorMessages } from "../GlobalMessage";
+import { editTag, tagActionTypes } from "../../modules/TagModule";
 
 const { useState } = React
 
@@ -13,12 +16,30 @@ interface Props {
   tag: Tag
 }
 
-
 const EditingTagModal = React.forwardRef((props: Props, ref: any) => {
+  const dispatch = useDispatch()
+
+  const [tagName, setTagName] = useState<string>(props.tag.name)
+  // TODO: description
+  const [tagDescription, setTagDescription] = useState('sample description')
   const [hex, setHex] = useState<string>(props.tag.color)
 
-  const handleSubmit = (event: React.FormEvent) => {}
-  const handleChange = (event: React.ChangeEvent) => {}
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTagName(event.currentTarget.value)
+  }
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault()
+
+    updateTag({ id: props.tag.id, name: tagName, color: hex, description: tagDescription })
+      .then((tag: Tag) => {
+        dispatch(editTag(tagActionTypes.update, tag))
+        successMessage(succesmMessages.update)
+      })
+      .catch(() => {
+        errorMessage(errorMessages.update)
+      })
+  }
 
   return (
     <React.Fragment>
