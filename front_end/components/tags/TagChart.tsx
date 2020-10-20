c9aab1f98957340caf7afc17503b476265056c7b
kazuki <matchingserver@gmail.com>
2020-10-21 03:14:33 +0900

    update color

--- front_end/components/tags/TagChart.tsx
+++ front_end/components/tags/TagChart.tsx
@@ -15,12 +15,15 @@ const TagChart: React.FC = () => {
       <Grid container spacing={4} alignItems="flex-start">
         <Grid item>
           <div style={circle}>1</div>
-        </Grid>
-        <Grid item>
+          <div>Expenditure Rate</div>
           <div style={circle}>2</div>
+          <div>Income Rate</div>
         </Grid>
         <Grid item>
           <div style={circle}>3</div>
+          <div>Fixed Expenditure Rate</div>
+          <div style={circle}>4</div>
+          <div>Total Rate</div>
         </Grid>
       </Grid>
     </React.Fragment>
