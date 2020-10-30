e57f94301103ef04b41fd0c8da04686e9fcc7c6d
kazuki <matchingserver@gmail.com>
2020-10-31 02:39:03 +0900

    return paid_at or earned_at when log is created by using callback

--- app/models/expenditure_log.rb
+++ app/models/expenditure_log.rb
@@ -1,4 +1,8 @@
 class ExpenditureLog < ApplicationRecord
+  # レコード保存時、カラムはデフォルトでCURRENT_TIMESTAMPになるが、
+  # ActiveRecordオブジェクトには反映されないためシリアライズしてレスポンスを返す前にcallbackしている。
+  after_create { self.paid_at = created_at }
+
   # validations
   validates :amount, presence: true
   validates :title, presence: true, length: { minimum: 1, maximum: 100 }
