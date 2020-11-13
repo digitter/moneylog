2d3595a235944a402630853cc1d56b6db6c2a453
kazuki <matchingserver@gmail.com>
2020-11-13 23:28:45 +0900

    fix test

--- spec/system/income_logs_spec.rb
+++ spec/system/income_logs_spec.rb
@@ -11,16 +11,14 @@ feature 'Income Logs ページ', js: true, type: :system do
     find('.MuiTypography-root', text: 'Income Logs').click
     expect(page).to have_current_path('/income_logs')
 
-    find('.MuiButton-label', text: 'NEW').click
+    find_all('.MuiButton-label', text: 'NEW')[0].click
 
     title = 'タイトル'
     amount = 100000
-    content = '内容'
 
     within('form') do
       fill_in 'title', with: title
       fill_in 'amount', with: amount
-      fill_in 'content', with: content
     end
 
     click_button 'CREATE'
@@ -32,9 +30,8 @@ feature 'Income Logs ページ', js: true, type: :system do
       # 作成の確認
       expect(tds[1]).to have_content title
       expect(tds[2]).to have_content amount
-      expect(tds[3]).to have_content content
-      within(tds[4]) { expect(find_field('in-log-earned-at').value).to eq Time.zone.now.strftime("%F") }
-      within(tds[7]) { find(:css, "svg.MuiSvgIcon-root").click }
+      within(tds[3]) { expect(find_field('in-log-earned-at').value).to eq Time.zone.now.strftime("%F") }
+      within(tds[6]) { find(:css, "svg.MuiSvgIcon-root").click }
     end
 
     within('.MuiDialogActions-root.MuiDialogActions-spacing') { find_button('Agree・はい').click }
@@ -42,7 +39,5 @@ feature 'Income Logs ページ', js: true, type: :system do
     # 削除の確認
     expect(page).not_to have_content(title)
     expect(page).not_to have_content(amount)
-    expect(page).not_to have_content(content)
   end
 end
-
