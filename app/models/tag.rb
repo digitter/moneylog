1ba89e04c5ac0066fde9f7d357326b5d7d27617e
kazuki <matchingserver@gmail.com>
2020-11-15 03:25:41 +0900

    change tag model dependent option destory to delete_all

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,7 @@ class Tag < ApplicationRecord
   # create update時
   # before_save { self.color = '#' + color } // TODO: 正規表現 ?
 
-  has_many :tag_relations, dependent: :destroy
+  has_many :tag_relations, dependent: :delete_all
   has_many :expenditure_logs, through: :tag_relations
   has_many :income_logs, through: :tag_relations
   belongs_to :user
