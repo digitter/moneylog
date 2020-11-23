1c4548943837ea0935d7fdcac843f11f29dc5b5a
kazuki <matchingserver@gmail.com>
2020-11-24 01:15:33 +0900

    hotfix: session store setting

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,16 @@
-Rails.application.config.session_store(
-  :cookie_store,
-  key: 'staging_app_session',
-  domain: 'digitter.info',
-  # domain: :all,
-  # same_site: :none
-)
+if Rails.env == "staging"
+  Rails.application.config.session_store(
+    :cookie_store,
+    key: 'staging_app_session',
+    domain: 'digitter.info',
+    # domain: :all,
+    # same_site: :none
+  )
+else
+  Rails.application.config.session_store(
+    :cookie_store,
+    key: 'development_app_session',
+  )
+end
 
 Rails.application.config.api_only = false
