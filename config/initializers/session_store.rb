f97756fe27b14a1da6ebdbe9167f8452caf3a34b
kazuki <matchingserver@gmail.com>
2021-01-02 02:05:21 +0900

    httponly and same_site setting

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,16 +1,20 @@
-if Rails.env == "staging"
+# APIモードだとsession storeが利用できないのでfalse指定
+Rails.application.config.api_only = false
+
+if Rails.env.staging?
   Rails.application.config.session_store(
     :cookie_store,
     key: 'staging_app_session',
     domain: 'digitter.info',
-    # domain: :all,
-    # same_site: :none
+    httponly: true,
+    same_site: :strict
   )
 else
   Rails.application.config.session_store(
     :cookie_store,
     key: 'development_app_session',
+    httponly: true,
+    same_site: :strict
   )
 end
 
-Rails.application.config.api_only = false
