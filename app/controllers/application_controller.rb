85497bcec10cd29363384dd4210d34e0779cc97b
kazuki <matchingserver@gmail.com>
2021-03-04 21:35:16 +0900

    return new auth token when invalid request occured

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,22 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
   include CurrentUserConcern
   include ResponseHelper
+
   before_action :verify_csrf_token
   after_action :set_csrf_token
 
   private
-    # トークン検証
+    # 認証トークン検証をし、不一致ならばセッションをリセット
     def verify_csrf_token
-      # 認証トークンが不一致ならばセッションをリセット
-      reset_session unless session[:auth_token] == request.headers['X-CSRF-Token']
-    end
+      unless session[:auth_token] == request.headers['X-CSRF-Token']
+        reset_session
 
-    # 認可チェック
-    def authorize_user!
-      # ユーザーがログインしていないならば401
-      response_unauthorized unless @current_user
+        # 400、500番台レスポンスエラー時には
+        # after_action が動作しない為、新しいトークンを送る必要がある.
+        set_csrf_token
+
+        response_unauthorized
+      end
     end
 
     # 新しいトークンをsession storeのcookieで管理しつつ、べつのcookieでset cookieする
@@ -23,7 +25,13 @@ class ApplicationController < ActionController::API
       session[:auth_token] = SecureRandom.urlsafe_base64
       cookies['csrf-token'] = {
         value: session[:auth_token],
-        domain: Rails.env.staging? ? 'digitter.info' : 'moneylog.com'
+        domain: Rails.application.credentials.dig(:host, :front)
       }
+    end
+
+    # 認可チェック
+    def authorize_user!
+      # ユーザーがログインしていないならば401
+      response_unauthorized unless @current_user
     end
 end
